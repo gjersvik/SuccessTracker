f9e91f2936cd8dcb1bfdcf816f1333469ad53edb
Ole Martin Gjersvik <olem@olem.org>
2013-08-17 18:37:27 +0200

    Updated store

--- lib/src/store.dart
+++ lib/src/store.dart
@@ -1,7 +1,9 @@
 part of success;
 
+class KeyNotFound{}
+
 abstract class Store{
-  Future<Entity> load(String name);
+  Future<Entity> load(String key);
   Future<Entity> save(Entity entity);
   Stream<Entity> get onChange;
 }
