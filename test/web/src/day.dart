19c23b863dde866280aba7f818e87220e5e7be26
Ole Martin Gjersvik <olem@olem.org>
2013-10-03 11:08:51 +0200

    PASS Day when .day is not 0 class has day.
    PASS Day when .day is not 0 class has day.

--- test/web/src/day.dart
+++ test/web/src/day.dart
@@ -6,7 +6,19 @@ dayTest() => group('Day', (){
     expect(day.elem.classes.contains('big'),isFalse);
     day.focus = true;
     expect(day.elem.classes.contains('big'),isTrue);
-    day.focus = true;
-    expect(day.elem.classes.contains('big'),isTrue);
+  });
+  
+  test('.elem.text starts wit .day and : .',(){
+    var day = new Day();
+    day.day = 1;
+    expect(day.elem.text,startsWith('1: '));
+    day.day = 27;
+    expect(day.elem.text,startsWith('27: '));
+  });
+
+  test('when .day is not 0 class has day',(){
+    var day = new Day();
+    day.day = 27;
+    expect(day.elem.classes.contains('day'), isTrue);
   });
 });
--- web/src/day.dart
+++ web/src/day.dart
@@ -3,8 +3,21 @@ part of success_tacker;
 class Day{
   final Element elem = new Element.div();
   
-  int day;
+  int _day = 0;
   int successes;
+  
+  int get day => _day;
+  set day(int d){
+    if(d == _day){
+      return;
+    }
+    _day = d;
+    elem.classes.toggle('day', d != 0);
+    _setText(_day,'');
+  }
+  
   bool get focus => elem.classes.contains('big');
   set focus(bool f) => elem.classes.toggle('big', f);
+  
+  _setText(day,succsess) => elem.text = '$day: succsess';
 }
